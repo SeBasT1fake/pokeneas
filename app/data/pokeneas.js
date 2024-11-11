244f62c5d19f341861d835486421f7e90c942192
SeBasT1fake <js.acarballo2@gmail.com>
2024-11-11 17:00:24 -0500

    adicion de pokeneas

--- app/data/pokeneas.js
+++ app/data/pokeneas.js
@@ -1,7 +1,7 @@
 const pokeneas = [
     {
         id: 1,
-        nombre: 'Pokenea1',
+        nombre: 'Sopleasur',
         altura: '1.2 m',
         habilidad: 'Sople masivo',
         imagen: 'https://storage.googleapis.com/tu-bucket/pokenea1.png',
@@ -9,13 +9,76 @@ const pokeneas = [
     },
     {
         id: 2,
-        nombre: 'Pokenea2',
+        nombre: 'Pepapod',
         altura: '0.8 m',
-        habilidad: 'Saltador',
+        habilidad: 'Extacido',
         imagen: 'https://storage.googleapis.com/tu-bucket/pokenea2.png',
-        frase: 'Saltar es la clave para alcanzar tus sueños.'
+        frase: 'Una vez al año no hace daño.'
+    },
+    {
+        id: 3,
+        nombre: "Flamequillo",
+        altura: "1.5 m", 
+        habilidad: "Fuego eterno",
+        imagen: "https://storage.googleapis.com/tu-bucket/pokenea3.png",
+        frase: "El fuego nunca se apaga si tienes pasión."
+    },
+    {
+        id: 4,
+        nombre: "Acuapuff",
+        altura: "1.0 m",
+        habilidad: "Nube de agua", 
+        imagen: "https://storage.googleapis.com/tu-bucket/pokenea4.png",
+        frase: "La vida es mejor bajo la lluvia."
+    },
+    {
+        id: 5,
+        nombre: "Terrapato",
+        altura: "0.5 m",
+        habilidad: "Escudo de tierra",
+        imagen: "https://storage.googleapis.com/tu-bucket/pokenea5.png",
+        frase: "Soy la roca en la que te puedes apoyar."
+    },
+    {
+        id: 6,
+        nombre: "Electrizor",
+        altura: "1.3 m",
+        habilidad: "Carga eléctrica",
+        imagen: "https://storage.googleapis.com/tu-bucket/pokenea6.png",
+        frase: "La energía nunca se detiene."
+    },
+    {
+        id: 7,
+        nombre: "Sombracielos",
+        altura: "2.0 m",
+        habilidad: "Niebla oscura",
+        imagen: "https://storage.googleapis.com/tu-bucket/pokenea7.png",
+        frase: "A veces, la oscuridad es el mejor refugio."
+    },
+    {
+        id: 8,
+        nombre: "Chamberder",
+        altura: "1.1 m",
+        habilidad: "Cegado eterno",
+        imagen: "https://storage.googleapis.com/tu-bucket/pokenea8.png",
+        frase: "Lo barato sale caro."
+    },
+    {
+        id: 9,
+        nombre: "Tussieve",
+        altura: "1.4 m",
+        habilidad: "Coctel narcotico",
+        imagen: "https://storage.googleapis.com/tu-bucket/pokenea9.png",
+        frase: "Pruebe que eso pega rico."
+    },
+    {
+        id: 10,
+        nombre: "Basuchu",
+        altura: "1.6 m",
+        habilidad: "Lanzamiento de basuco",
+        imagen: "https://storage.googleapis.com/tu-bucket/pokenea10.png",
+        frase: "Basuco impacto."
     }
-    // Agregar más Pokeneas
 ];
 
 module.exports = pokeneas;
