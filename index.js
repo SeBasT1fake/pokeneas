140f2828aeddcc8292e998dd25ac5a55d2331b18
SeBasT1fake <js.acarballo2@gmail.com>
2024-11-12 13:03:43 -0500

    cambio puertos

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const pokeneaRoutes = require('./app/routes/pokeneaRoutes');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
